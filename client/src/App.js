73ca528b6506b3e27913db04cf2195106999d758
Gizmo10 <sw.ndzala@gmail.com>
2023-08-29 16:58:57 +0200

    Added css for login UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import './App.css';
+import PatientLogin from "./pages/PatientLogin";
+import PatientRegister from "./pages/PatientRegister";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <h1>Hey There!!</h1>
+    <ToastContainer position="top-center" />
+    <Routes>
+      <Route exact path="/" element= { <PatientLogin/> } />
+      <Route path="/PatientRegister" element= { <PatientRegister/> } />
+    </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/PatientLogin.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import './PatientLogin.css';
+
+const initialState = {
+    name: "",
+    surname: "",
+    identityNumber: "",
+}
+
+const PatientLogin = () => {
+
+    const [state, setState] = useState(initialState);
+    const {name, surname, identityNumber} = state;
+
+    const navigate = useNavigate();
+
+    const handleInputChange = (e) => {
+        const { state, value} = e.target;
+        setState({...state, [name] : value });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+  return (
+    <div class = "mainDiv">
+      <form 
+            onSubmit = { handleSubmit }
+      >
+        <label htmlFor="name">Name: </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Your name..."
+          value={name || ""}
+          onChange={handleInputChange}
+        />
+
+        <label htmlFor="surname">Surname: </label>
+        <input
+        type = "text"
+        id = "surname"
+        name = "surname"
+        placeholder = "Your surname..."
+        value = {surname || ""}
+        onChange = {handleInputChange}
+        />
+
+        <label htmlFor="identity"> Id/Passport: </label>
+        <input 
+        type = "text"
+        id = "identity"
+        name = "identity"
+        placeholder = "Your ID/Passport number..."
+        value = {identityNumber || ""}
+        onChange = {handleInputChange}
+        />
+
+        <input type="submit" value="Login" />
+       <Link  to="/PatientRegister">
+            <input id ="register" type="submit"  value="Register" />
+       </Link>
+
+      </form>
+    </div>
+  )
+}
+
+export default PatientLogin;
