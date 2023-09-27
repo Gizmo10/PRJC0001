da8b549653bf89598d7847acf6da44e239acaf08
Gizmo10 <sw.ndzala@gmail.com>
2023-09-27 15:00:27 +0200

    Fixed login UI input fields

--- client/src/pages/PatientLogin.js
+++ client/src/pages/PatientLogin.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify"
 import axios from "axios";
 import './PatientLogin.css';
 
@@ -8,23 +9,40 @@ const initialState = {
     surname: "",
     identityNumber: "",
     password: "",
-}
+};
 
 const PatientLogin = () => {
 
     const [state, setState] = useState(initialState);
     const {name, surname, identityNumber, password} = state;
 
+    const name_surname_pattern = /^[A-Z]{1}([a-z]+)$/;
+    const identity_pattern = /[0-9]{6,13}/; 
+
+
     const navigate = useNavigate();
 
     const handleInputChange = (e) => {
-        const { state, value} = e.target;
+        const { name, value} = e.target;
         setState({...state, [name] : value });
-    }
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-    }
+
+        if(!name || !surname || !identityNumber || !password) {
+          toast.error("One of the fields is missing a value.");
+        } else{
+           if(name_surname_pattern.test(name) && name_surname_pattern.test(surname) && identity_pattern.test(identityNumber)) {
+            toast.success("Regular Expression meets stanadrd")
+
+           } else {
+            toast.error("Doesnt match pattern");
+           }
+        }
+
+    };
+
   return (
     <div className = "mainDiv">
       <form 
@@ -77,7 +95,7 @@ const PatientLogin = () => {
 
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default PatientLogin;
